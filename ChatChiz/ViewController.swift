2c78bca039d2bc0ecde645e2d098dc8e41c357a8
ngocban284 <daongocban1102@gmail.com>
2022-09-08 21:07:47 +0700

    Initial Commit

---
+++ ChatChiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatChiz
+//
+//  Created by Ngoc Ban on 08/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
